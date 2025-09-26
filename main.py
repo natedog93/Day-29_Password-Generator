e643cc862c7ee5734773a4fe10378bcff60f36c5
nmojica <nmlogon2700@gmail.com>
2025-09-25 23:00:24 -0700

    Finalized generate password button.

--- main.py
+++ main.py
@@ -9,29 +9,22 @@ YELLOW = "#f7f5dd"
 FONT_NAME = "Helvetica"
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+def generate_random_password():
 
-nr_letters = random.randint(8, 10)
-nr_symbols = random.randint(2, 4)
-nr_numbers = random.randint(2, 4)
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-password_list = []
+    password_list = []
+    [password_list.append(random.choice(letters)) for letter in range(random.randint(8,10))]
+    [password_list.append(random.choice(symbols)) for symbol in range(random.randint(2,4))]
+    [password_list.append(random.choice(numbers)) for number in range(random.randint(2, 4))]
+    random.shuffle(password_list)
 
-[password_list.append(random.choice(letters)) for char in range(nr_letters)]
+    password = "".join(password_list)
 
-[password_list.append(random.choice(symbols)) for char in range(nr_symbols)]
-
-[password_list.append(random.choice(numbers)) for char in range(nr_numbers)]
-
-random.shuffle(password_list)
-
-password = ""
-for char in password_list:
-  password += char
-
-print(f"Your password is: {password}")
+    password_input.delete(0, END)  # Delete all existing text
+    password_input.insert(0, password)
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -91,7 +84,7 @@ password_input = Entry(show="*", width= 33)
 password_input.grid(column = 1, row = 3)
 
 #Buttons
-generate_password_button = Button(text= "Generate Password")
+generate_password_button = Button(text= "Generate Password", command=generate_random_password)
 generate_password_button.grid(column = 2, row = 3)
 
 add_button = Button(text="Add", width = 44, command=save)
